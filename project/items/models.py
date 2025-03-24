4466b1956802a041a359b1722fde9bf4e51070cd
WrldreaX4 <157551879+WrldreaX4@users.noreply.github.com>
2025-03-24 21:47:24 +0800

    activity4

--- project/items/models.py
+++ project/items/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    enrolled_date = models.DateField(auto_now_add=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)  # I add New field phone number
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    
